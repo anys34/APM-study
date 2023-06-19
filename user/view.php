41ef1e8c92af7f0d97233d456c561353e44ab1af
anys34 <anyeseong34@gmail.com>
2023-06-19 20:12:40 +0900

    6/19

--- user/view.php
+++ user/view.php
@@ -5,13 +5,15 @@ $result = mysqli_query($conn, $query);          // 쿼리 실행
 $count = mysqli_num_rows($result);
 
 echo "<table border=1 width = 500 text align=center>";
-echo "<tr align=center><td colspan=5> 데이터의 개수는 ".$count."개 입니다 <br></td></tr>";
-echo "<tr><th>번호</th><th>이름</th><th>나이</th><th>성별</th><th>직업</th></tr>";
+echo "<tr align=center><td colspan=7> 데이터의 개수는 ".$count."개 입니다 <br></td></tr>";
+echo "<tr><th>번호</th><th>이름</th><th>나이</th><th>성별</th><th>직업</th><th>삭제</th><th>수정</th></tr>";
 
 
 while ($row = mysqli_fetch_assoc($result)) {         
     echo "<tr align=center><td>" . $row["num"] . "</td><td>" . $row["name"] . "</td><td>" . $row["age"] . "</td>
-    <td>" . $row["gender"] . "</td><td>" . $row["work"] . "</td></tr>";                   // 결과 출력
+    <td>" . $row["gender"] . "</td><td>" . $row["work"] . "</td>
+    <td><a href=delete.php?num=".$row["num"].">삭제</a></td>
+    <td><a href=modify.php?num=".$row["num"].">수정</a></td></tr>";                   // 결과 출력
 }
 echo "</table>";                                                       // MySQL 연결 닫기
 ?>
